e84661c6a8e51ee982bd56760703c840a409bc80
Jesse Farmer <jessethebuilder@yahoo.com>
2016-07-26 01:26:51 -0700

    'debug'

--- lib/farm_scrape/js_scrape.rb
+++ lib/farm_scrape/js_scrape.rb
@@ -6,7 +6,7 @@ class JsScrape
   attr_accessor :ghost
 
   def initialize(proxy: false, timeout: 120, phantomjs: nil)
-    build_ghost(random_proxy, :options => {:timeout => timeout, :phantomjs => phantomjs, :debug => true})
+    build_ghost(random_proxy, :options => {:timeout => timeout, :phantomjs => phantomjs, debug: true})
   end
 
   def JsScrape.with_random_proxy
