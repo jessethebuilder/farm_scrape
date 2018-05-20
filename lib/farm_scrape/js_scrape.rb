fd16ca14bb4b6cc952ade61369734b27eaac7d1b
Jesse Farmer <jessethebuilder@yahoo.com>
2018-05-19 18:22:09 -0700

    123

--- lib/farm_scrape/js_scrape.rb
+++ lib/farm_scrape/js_scrape.rb
@@ -6,9 +6,11 @@ class JsScrape
   attr_accessor :ghost
 
   def initialize(proxy: false, timeout: 120, phantomjs: nil, debug: false, phantomjs_debug: false, phantomjs_options: nil)
+    @proxy = proxy
     @debug = debug
-    build_ghost(:options => {timeout: timeout, phantomjs: phantomjs,
-                             debug: phantomjs_debug, phantomjs_options: phantomjs_options})
+    @options = {timeout: timeout, phantomjs: phantomjs,
+                debug: phantomjs_debug, phantomjs_options: phantomjs_options}
+    build_ghost
   end
 
   def page
@@ -59,11 +61,11 @@ class JsScrape
     puts "FarmScrape: #{msg}"
   end
 
-  def build_ghost(proxy, options: {})
-    if proxy
-      GhostMaker.new({phantomjs_options: ["--proxy=#{proxy[:ip]}:#{proxy[:port]}"]}.merge(options))
+  def build_ghost
+    if @proxy
+      GhostMaker.new({phantomjs_options: ["--proxy=#{proxy[:ip]}:#{proxy[:port]}"]}.merge(@options))
     else
-      GhostMaker.new(options)
+      GhostMaker.new(@options)
     end
 
     @ghost = Capybara::Session.new(:farm_poltergeist)
@@ -77,9 +79,6 @@ class GhostMaker
                phantomjs_options: phantomjs_options}
     options[:phantomjs] = phantomjs if phantomjs
 
-    # Capybara.register_driver :farm_poltergeist do |app|
-    #   Capybara::Poltergeist::Driver.new(app, options)
-    # end
     Capybara.register_driver :farm_poltergeist do |app|
       Capybara::Poltergeist::Driver.new(app, options)
     end
